8c5a45b0596cb1e5e8f3b5aced48b6138eb11c75
EmperorPenguin18 <60635017+EmperorPenguin18@users.noreply.github.com>
2023-11-12 18:48:27 -0500

    Basic keybinds

--- src/av.cc
+++ src/av.cc
@@ -5,6 +5,8 @@
 #include <algorithm>
 
 #include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
 using namespace rapidjson;
 
 //temp
@@ -181,9 +183,17 @@ const char* example_json =
 	\"cuts\": []\
 }";
 
+enum Mode {
+	NORMAL_MODE,
+	CONSIDERING_MODE,
+	SCRYFALL_MODE,
+	NUM_MODES,
+	INVALID_MODE
+};
+
 class AetherVortex {
 	public:
-		AetherVortex(Document* doc) {
+		AetherVortex(Document& doc) : m_doc(doc) {
 			initscr();
 			noecho();
 			keypad(stdscr, TRUE);
@@ -191,8 +201,8 @@ class AetherVortex {
 			m_running = true;
 			m_pos_y = 0;
 			m_pos_x = 0;
-			m_doc = doc;
 			m_column_gap = 10;
+			m_mode = NORMAL_MODE;
 		}
 		~AetherVortex() {
 			endwin();
@@ -201,46 +211,143 @@ class AetherVortex {
 			while (m_running) {
 				int max_y, max_x;
 				getmaxyx(stdscr, max_y, max_x);
-				const Value& a = (*m_doc)["cards"];
+				const Value& a = m_doc["cards"];
 				assert(a.IsArray());
 				draw(a, max_y, max_x);
-				events(a, max_y, max_x);
+				events(max_y, max_x);
 			}
 		}
 	private:
 		bool m_running;
 		int m_pos_y;
 		int m_pos_x;
-		Document* m_doc;
+		Document& m_doc;
 		int m_rows;
 		int m_columns;
 		int m_column_gap;
+		Mode m_mode;
 
-		void events(const Value& a, const int max_y, const int max_x) {
+		void events(const int max_y, const int max_x) {
 			char c = getch();
+			int i = (max_y*m_pos_x) + m_pos_y;
 			switch (c) {
 				case 'q':
+					{
 					m_running = false;
 					break;
+					}
+				case 'i':
+					{
+					if (m_doc["cards"].Size() > 0) {
+						m_doc["considering"].PushBack(m_doc["cards"][i], m_doc.GetAllocator());
+						m_doc["cards"].Erase(m_doc["cards"].Begin()+i);
+						m_pos_y -= 1;
+						if (m_pos_y < 0) {
+							m_pos_y = max_y-1;
+							m_pos_x -= 1;
+						}
+						m_mode = CONSIDERING_MODE;
+					}
+					break;
+					}
+				case 'o':
+					{
+					m_mode = SCRYFALL_MODE;
+					break;
+					}
+				case 'a':
+					{
+					m_mode = CONSIDERING_MODE;
+					break;
+					}
+				case 'd':
+					{
+					if (m_doc["cards"].Size() > 0) {
+						m_doc["cuts"].PushBack(m_doc["cards"][i], m_doc.GetAllocator());
+						m_doc["cards"].Erase(m_doc["cards"].Begin()+i);
+						m_pos_y -= 1;
+						if (m_pos_y < 0) {
+							m_pos_y = max_y-1;
+							m_pos_x -= 1;
+						}
+					}
+					break;
+					}
+				case 'g':
+					{
+					m_pos_y = 0;
+					m_pos_x = 0;
+					break;
+					}
+				case 'h':
+					{
+					if (m_pos_x > 0) m_pos_x -= 1;
+					break;
+					}
 				case 'j':
+					{
 					if (m_pos_x == m_columns) {
 						if (m_pos_y < m_rows) m_pos_y++;
 					} else {
 						if (m_pos_y < max_y-1) m_pos_y++;
 					}
 					break;
+					}
 				case 'k':
+					{
 					if (m_pos_y > 0) m_pos_y -= 1;
 					break;
+					}
 				case 'l':
+					{
 					if (m_pos_x < m_columns) {
 						m_pos_x++;
 						if (m_pos_y > m_rows) m_pos_y = m_rows;
 					}
 					break;
-				case 'h':
-					if (m_pos_x > 0) m_pos_x -= 1;
+					}
+				case 'z':
+					{
+					//temp
+					StringBuffer buffer;
+					buffer.Clear();
+					Writer<StringBuffer> writer(buffer);
+					m_doc.Accept(writer);
+					fprintf(stderr, "%s", buffer.GetString());
 					break;
+					}
+				case 'x':
+					{
+					if (m_doc["cards"].Size() > 0) {
+						m_doc["considering"].PushBack(m_doc["cards"][i], m_doc.GetAllocator());
+						m_doc["cards"].Erase(m_doc["cards"].Begin()+i);
+						m_pos_y -= 1;
+						if (m_pos_y < 0) {
+							m_pos_y = max_y-1;
+							m_pos_x -= 1;
+						}
+					}
+					break;
+					}
+				case 'c':
+					{
+					if (m_doc["cards"].Size() > 0) {
+						m_doc["considering"].PushBack(m_doc["cards"][i], m_doc.GetAllocator());
+						m_doc["cards"].Erase(m_doc["cards"].Begin()+i);
+						m_pos_y -= 1;
+						if (m_pos_y < 0) {
+							m_pos_y = max_y-1;
+							m_pos_x -= 1;
+						}
+						m_mode = SCRYFALL_MODE;
+					}
+					break;
+					}
+				case 27: //Esc
+					{
+					m_mode = NORMAL_MODE;
+					break;
+					}
 				default:
 					break;
 			}
@@ -278,7 +385,7 @@ class AetherVortex {
 int main(int argc, char** argv) {
 	Document doc;
 	doc.Parse(example_json); //temp
-	AetherVortex av(&doc);
+	AetherVortex av(doc);
 	av.run();
 	return 0;
 }
